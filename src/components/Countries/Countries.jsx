1a19e7fc495fd3cf1c8b85bef19f309e8728adfb
razuahmeds <rukunuzzaman.razu@gmail.com>
2026-09-15 01:10:26 +0600

    style css add country visited add

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
@@ -10,11 +11,13 @@ const Countries = ({countriesPromise}) => {
     return (
         <div>
             <h1>countries...{countries.length}</h1>
-            {
-                countries.map(country => <Country
+            <div className='countries'>
+                {
+                    countries.map(country => <Country
                     key={country.cca3.cca3}
                     country={country}></Country>)
-            }
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
+
+    const [visited, setVisited] = useState(false);
+
+
+
     // console.log(country.population.population);
-    console.log(country.languages);
+    // console.log(country.languages.languages);
+
+
+    const handleVisited = () =>{
+
+        // system 1
+        if(visited){
+            setVisited(false)
+        }
+        else{
+            setVisited(true)
+        }
+
+        // system 2
+        // setVisited(visited ? false : true);
+
+
+        // system 3
+        // setVisited(!visited)
+        
+        // console.log('button clicked');
+        
+    }
     
     
     return (
-        <div>
-            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+        // <div className={`country border-lg tex-center ${visited ? 'country-visited' : 'country-not-visited'}`}>
+        <div className={`country ${visited && 'country-visited'}`}>
+            <img className='flags-img' src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
-            <h3>Area: {country.area.area}</h3>
+            {/* <h3>Area: {country.area.area}</h3> */}
+            <h3>Area: {country.area.area} {country.area.area >30000 ? "Big country" : "Small country"}</h3>
+            {/* <h3>languages: {country.languages.languages}</h3> */}
             <h4>Capital: {country.capital.capital}</h4>
             <h5>Continents: {country.continents.continents}</h5>
             <p>Population: {country.population.population}</p>
+            <button className='country-btn' onClick={handleVisited}>
+                   {visited ? 'Visited' : 'Not Visited'}
+                </button>
         </div>
     );
 };
