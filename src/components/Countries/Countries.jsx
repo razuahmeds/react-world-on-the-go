0f030d5c5a7905422b155dd5169b60686aa25298
razuahmeds <rukunuzzaman.razu@gmail.com>
2026-09-14 15:26:46 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Hello...</h1>
+            <h2>React world on the go....</h2>
+        </div>
+    );
+};
+
+export default Countries;
