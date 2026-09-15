0e7ff9266487874af69b49aaf0f504fb7f19796b
razuahmeds <rukunuzzaman.razu@gmail.com>
2026-09-16 01:02:18 +0600

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 
-import { Suspense } from 'react'
-import './App.css'
-import Countries from './components/Countries/Countries'
+import { Suspense } from 'react';
+import './App.css';
+import Countries from './components/Countries/Countries';
 
-
-
-const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
-  .then(res => res.json())
+const countriesPromise = fetch(
+    'https://openapi.programming-hero.com/api/all'
+)
+    .then(res => res.json());
 
 function App() {
 
-  return (
-    <>
-    <Suspense fallback={<p>Nadir vai loading...</p>}>
-      <Countries countriesPromise ={countriesPromise}></Countries>
-    </Suspense>
-    </>
-  )
+    return (
+        <>
+            <Suspense fallback={<p>Nadir vai loading...</p>}>
+                <Countries
+                    countriesPromise={countriesPromise}
+                />
+            </Suspense>
+        </>
+    );
 }
 
-
-export default App
+export default App;
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,23 +1,80 @@
-import React, { use } from 'react';
+
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
-import './Countries.css'
+import './Countries.css';
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+    // Add visited country
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    };
+
+    // Add visited flag
+    const handleVisitedFlags = (flag) => {
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    };
+
     const countriesData = use(countriesPromise);
-    const countries = countriesData.countries
+    const countries = countriesData.countries;
 
-    console.log(countries);
-    
     return (
         <div>
-            <h1>countries...{countries.length}</h1>
-            <div className='countries'>
+
+            <h1>Countries... {countries.length}</h1>
+
+            <h3>
+                Total country visited: {visitedCountries.length}
+            </h3>
+
+            <h3>
+                Total flags visited: {visitedFlags.length}
+            </h3>
+
+            {/* Visited Countries */}
+            <ol>
                 {
-                    countries.map(country => <Country
-                    key={country.cca3.cca3}
-                    country={country}></Country>)
+                    visitedCountries.map((country) => (
+                        <li key={country.cca3.cca3}>
+                            {country.name.common}
+                        </li>
+                    ))
+                }
+            </ol>
+
+            {/* Visited Flags */}
+            <div>
+                {
+                    visitedFlags.map((flag, index) => (
+                        <img
+                            key={index}
+                            src={flag}
+                            alt="Visited country flag"
+                            width="100"
+                        />
+                    ))
                 }
             </div>
+
+            {/* All Countries */}
+            <div className="countries">
+                {
+                    countries.map((country) => (
+                        <Country
+                            key={country.cca3.cca3}
+                            country={country}
+                            handleVisitedCountries={handleVisitedCountries}
+                            handleVisitedFlags={handleVisitedFlags}
+                        />
+                    ))
+                }
+            </div>
+
         </div>
     );
 };
